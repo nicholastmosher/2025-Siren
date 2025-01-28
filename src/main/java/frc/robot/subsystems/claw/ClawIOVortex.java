ab8d47cbf680b85b28639239fc090633173f1cdf
ALARM <alarmrobotixs2079@gmail.com>
2025-01-27 19:37:31 -0500

    worked on wrist

--- src/main/java/frc/robot/subsystems/claw/ClawIOVortex.java
+++ src/main/java/frc/robot/subsystems/claw/ClawIOVortex.java
@@ -1,6 +1,10 @@
 package frc.robot.subsystems.claw;
 
 import org.littletonrobotics.junction.Logger;
+
+import com.revrobotics.spark.SparkBase;
+import com.revrobotics.spark.SparkBase.ControlType;
+import com.revrobotics.spark.SparkClosedLoopController;
 import com.revrobotics.spark.SparkFlex;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 
@@ -9,6 +13,7 @@ import edu.wpi.first.units.measure.Velocity;
 public class ClawIOVortex implements ClawIO {
     
     private final SparkFlex motor;
+    private final SparkClosedLoopController m_controller;
 
     // Current wheel speed (for logging or feedback)
     private double currentSpeed = 0.0;
@@ -16,18 +21,25 @@ public class ClawIOVortex implements ClawIO {
     // Constructor
     public ClawIOVortex(int motorPort) {
         motor = new SparkFlex(motorPort, MotorType.kBrushless);
+        m_controller = motor.getClosedLoopController();
+
     }
 
-    @Override
-    public void setSpeed(Velocity speed) {
-
+    public void setSpeed(double speed) {
+       m_controller.setReference(speed, ControlType.kMAXMotionVelocityControl);
     }
 
     public double getCurrentSpeed() {
-        return 0;
+        return motor.getEncoder().getVelocity();
     }
 
     public void updateInputs(ClawIOInputs inputs) {
         
+    }
+
+    @Override
+    public void setSpeed(Velocity speed) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setSpeed'");
     }
 }
